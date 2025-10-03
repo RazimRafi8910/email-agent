70f88bb5463bc3287c1f7719f5933188731d6f07
RazimRafi8910 <8910razim@gmail.com>
2025-10-03 20:57:37 +0530

    fix:minor

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ app.post("/webhook", async (req, res) => {
 	console.log(body)
 	if (message?.from == undefined || message?.from != process.env.PHONE_NO) {
 		await sendMessage(`message from unkown ${message?.from}`);
-		return res.statusCode(200);
+		return res.sendStatus(200);
 	}
 
 	if (message && message.type === "text") {
